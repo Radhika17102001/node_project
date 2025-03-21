5a1030a851686b87fbb199fd882c3c1394b2c580
Radhika17102001 <radhika.lnu@rdpolytech.ca>
2025-03-20 21:30:21 -0500

    update

--- app.js
+++ app.js
@@ -1,34 +1,12 @@
-//this imports express
 const express = require('express');
 
-//use dotenv to expose our environment variables
-require('dotenv').config();
+const router = require("./routes/pages");
 
-//we create a new instance of express as the app object
 const app = express();
-//PORT assignment
-const PORT = process.env.PORT;
+const PORT = 3000;
 
-//router handler that will define uri/url that does something
-app.get('/home', (req, res)=>{
-    res.send('hello, world');
+// implementing routes
+app. use(router);
+app. listen (PORT, () => { 
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-//router handler that uses post. post creates resources
-
-app.post('/user/:id',(req, res) =>{
-    const user_id = req.params.id;
-    res.send(`The user id is: ${user_id}`);
-});
-
-app.get('/search', (req, res)=>{
-    const query = req.query.q;
-    res.send(`this is the search term entered: ${query}`);
-});
-
-
-//start our server
-app.listen(PORT, () =>{
-    console.log(`Server started on port: ${PORT}`);
-});
-
